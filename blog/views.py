fb351024b554e5f8ca9f7f0319cec8c4dd13dc65
myjungwu <baek.myungsuk@gmail.com>
2022-02-11 15:25:59 +0900

    views add

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# Views 내에 선언된 함수로 인자로 HttpRequest 라는 객체를 Django가 전달해준다.
+def post_list(request):
+    my_name = '장고웹프레임워크'
+    http_method = request.method
+    return HttpResponse('''
+        <h2>Welcome {name}</h2>
+        <p>Http Method : {method}</p>
+        <p>Http Encoding : {encod}</p>
+        <p>Http Path : {mypath}</p>        
+    '''.format(name=my_name, method=http_method, encod=request.encoding, mypath=request.path))
+
