deee54f7a13f746667c99e496e6311da0f6c0e85
myjungwu <baek.myungsuk@gmail.com>
2022-02-14 14:43:45 +0900

    post_detail method add

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
 
-# Create your views here.
+# 글상세정보
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 # Views 내에 선언된 함수로 인자로 HttpRequest 라는 객체를 Django가 전달해준다.
+# 글목록
 def post_list(request):
     my_name = '장고웹프레임워크'
     http_method = request.method
