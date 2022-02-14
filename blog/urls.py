fe5408fbd78b01f4944d8adaef819e88db09a7dc
myjungwu <baek.myungsuk@gmail.com>
2022-02-14 14:58:20 +0900

    post_detail method add

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/blog
     path('', views.post_list, name='post_list_home'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
 ]
