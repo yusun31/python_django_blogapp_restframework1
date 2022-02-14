77e29a5b18aa22f2840518cbed98f72a6d05f060
myjungwu <baek.myungsuk@gmail.com>
2022-02-14 09:58:23 +0900

    html add

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,13 @@ from django.shortcuts import render
 def post_list(request):
     my_name = '장고웹프레임워크'
     http_method = request.method
-    return HttpResponse('''
-        <h2>Welcome {name}</h2>
-        <p>Http Method : {method}</p>
-        <p>Http headers User-Agent : {header}</p>
-        <p>Http Path : {mypath}</p>        
-    '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path))
+
+    # return HttpResponse('''
+    #     <h2>Welcome {name}</h2>
+    #     <p>Http Method : {method}</p>
+    #     <p>Http headers User-Agent : {header}</p>
+    #     <p>Http Path : {mypath}</p>
+    # '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path))
+
+    return render(request, 'blog/post_list.html')
 
