cc3b25870c0fa0104d181ac024efa6bfe1861e12
myjungwu <baek.myungsuk@gmail.com>
2022-02-15 10:50:12 +0900

    base.html update

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list_home'),
     # http://localhost:8000/blog/post/5
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    # http://localhost:8000/blog/post/new
+    path('post/new', views.post_new, name='post_new'),
 ]
