afb067debc38f9478577a9277fd923701ea25832
myjungwu <baek.myungsuk@gmail.com>
2022-02-15 11:42:59 +0900

    views post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .models import Post
@@ -7,7 +7,22 @@ from .forms import PostModelForm
 
 # 글등록
 def post_new(request):
-    post_form = PostModelForm()
+    if request.method == 'POST':
+        # 등록을 요청하는 경우
+        post_form = PostModelForm(request.POST)
+        # form 객체의 save() 호출하면 Model 객체가 생성되어진다.
+        post = post_form.save(commit=False)
+        # 로그인된 username을 작성자(author)필드에 저장
+        post.author = request.user
+        # 현재날짜시간을 게시일자(published_date) 필드에 저장
+        post.published_date = timezone.now()
+        # post 객체가 저장되면서 insert 처리가 되어진다.
+        post.save()
+        # 등록 후에 상세페이지로 리다이렉션 처리하기
+        return redirect('post_detail', pk=post.pk)
+    else:
+        # 등록을 위한 Form을 출력하는 경우
+        post_form = PostModelForm()
     return render(request, 'blog/post_edit.html', {'postform': post_form})
 
 # 글상세정보
