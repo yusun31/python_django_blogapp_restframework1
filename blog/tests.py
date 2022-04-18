f544e8551c48b9e7fbc0f79e34f644c6505fdcef
yusun <yousun2286@naver.com>
2022-04-18 17:32:59 +0900

    test add

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def concate_two_strings(str1, str2):
+	return str1 + str2
+
+
+class TestExample(TestCase):
+    def test_concate_two_strings(self):
+        self.assertEqual(concate_two_strings("Hello, ", "How are you?"), "Hello, How are you?")
--- mydjango/settings.py
+++ mydjango/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db', # DB명
-        'USER': 'python', # 데이터베이스 계정
-        'PASSWORD':'python', # 계정 비밀번호
-        'HOST':'localhost', # 데이테베이스 IP
-        'PORT':'3306', # 데이터베이스 port
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'django_db', # DB명
+    #    'USER': 'python', # 데이터베이스 계정
+    #    'PASSWORD':'python', # 계정 비밀번호
+    #   'HOST':'localhost', # 데이테베이스 IP
+    #    'PORT':'3306', # 데이터베이스 port
+    #}
 }
 
 
