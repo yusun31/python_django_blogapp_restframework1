351f2f8e8c91bb948c1a3df6fd07332167d5b6bb
yusun <yousun2286@naver.com>
2022-04-21 11:55:52 +0900

    token

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
         'NAME': 'django_db',  # DB명
         'USER': 'python',  # 데이터베이스 계정
         'PASSWORD': 'python',  # 계정 비밀번호
-        'HOST': 'mysql-svc',  # 데이테베이스 IP
+        #'HOST': 'mysql-svc',  # 데이테베이스 IP
+        'HOST': 'localhost',
         'PORT': '3306',  # 데이터베이스 port
     }
 }
@@ -139,5 +140,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',)
+    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
